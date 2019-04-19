f8a1a51bf125d228ad4bc26bba62c018e57d3fd2
SUBASH KRISHNAN <starboy3712@gmail.com>
2019-04-19 20:52:02 +0530

    Firstt commit :)

---
+++ LoginAPI/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using LoginAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+        [HttpPost("CheckLogin")]
+        public int CheckLogin([FromBody]LoginModel login)
+        {
+            int status = 0;
+            if (login.username == "admin" && login.password == "1")
+            {
+                status = 1;
+            }
+            return status;
+        }
+    }
+}
---
+++ LoginAPI/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LoginAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public static IConfiguration StaticConfig { get; private set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            StaticConfig = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowAnyOrigin()
+                .AllowCredentials();
+            }));
+            services.AddMvc(options =>
+            {
+                options.RespectBrowserAcceptHeader = true;
+            });
+            services.Configure<FormOptions>(x =>
+            {
+                x.ValueLengthLimit = int.MaxValue;
+                x.MultipartBodyLengthLimit = int.MaxValue; // In case of multipart
+            });
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("MyPolicy");
+
+            app.UseAuthentication();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}");
+            });
+        }
+    }
+}
